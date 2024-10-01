a8c671caea9f1753fd69f710d64321fdb2d65136
Dhruv Shah <shahdhruvddd@gmail.com>
2024-10-02 01:05:57 +0530

    Update Loan.java

--- src/main/java/AdaptiveLibraryManagementSystem/Loan.java
+++ src/main/java/AdaptiveLibraryManagementSystem/Loan.java
@@ -9,30 +9,52 @@
 
 package AdaptiveLibraryManagementSystem;
 
+/**
+ * The Loan class represents a loan record in the library management system.
+ * It stores information about which member has borrowed which book.
+ */
 public class Loan {
-    private int id;
-    private final int bookId;
-    private final int memberId;
+    // Instance variables
+    private int id;                // Unique ID for the loan record
+    private final int bookId;      // ID of the book being borrowed
+    private final int memberId;    // ID of the member borrowing the book
 
+   // Constructs a Loan object with a specified loan ID, book ID, and member ID.
     public Loan(int id, int bookId, int memberId) {
-        this.id = id;
-        this.bookId = bookId;
-        this.memberId = memberId;
+        this.id = id;               // Initialize loan ID
+        this.bookId = bookId;       // Initialize book ID
+        this.memberId = memberId;   // Initialize member ID
     }
 
+    // Constructs a Loan object without specifying the loan ID (for creating new loan records).
     public Loan(int bookId, int memberId) {
-        this.bookId = bookId;
-        this.memberId = memberId;
+        this.bookId = bookId;       // Initialize book ID
+        this.memberId = memberId;   // Initialize member ID
     }
 
+    /**
+     * Gets the ID of the loan.
+     * 
+     * @return The loan ID.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the ID of the book being borrowed.
+     * 
+     * @return The book ID.
+     */
     public int getBookId() {
         return bookId;
     }
 
+    /**
+     * Gets the ID of the member borrowing the book.
+     * 
+     * @return The member ID.
+     */
     public int getMemberId() {
         return memberId;
     }
