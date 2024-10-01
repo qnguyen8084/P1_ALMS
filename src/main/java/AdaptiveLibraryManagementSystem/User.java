0782e8d2498d227e77e38774b5675bb2b956849f
Dhruv Shah <shahdhruvddd@gmail.com>
2024-10-02 01:09:04 +0530

    Update User.java

--- src/main/java/AdaptiveLibraryManagementSystem/User.java
+++ src/main/java/AdaptiveLibraryManagementSystem/User.java
@@ -9,52 +9,104 @@
 
 package AdaptiveLibraryManagementSystem;
 
+/**
+ * The User class is an abstract base class that represents a user in the library system.
+ * It holds the basic attribute 'name' for any type of user (e.g., member, employee).
+ */
 public abstract class User {
+    // Name of the user
     private final String name;
 
+    /**
+     * Constructor to initialize the name of the user.
+     * 
+     * @param name The name of the user.
+     */
     public User(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name of the user.
+     * 
+     * @return The user's name.
+     */
     public String getName() {
         return name;
     }
-
 }
 
+/**
+ * The FetchId interface declares a method for fetching the ID of a user.
+ * It is implemented by classes that represent specific types of users with unique IDs.
+ */
 interface FetchId {
+    /**
+     * Gets the unique ID of the user.
+     * 
+     * @return The user's ID.
+     */
     int getId();
 }
 
+/**
+ * The Member class represents a member of the library, inheriting from the User class
+ * and implementing the FetchId interface to provide a unique member ID.
+ */
 class Member extends User implements FetchId {
+    // Unique ID for the member
     private int memberId;
 
+    //Constructor to create a Member object with just the name (used when the memberId is not yet known).
+     
     public Member(String name) {
-        super(name);
+        super(name);  // Call the User class constructor to set the name
     }
 
+    // Constructor to create a Member object with both the member ID and name.
+     
     public Member(int memberId, String name) {
-        super(name);
-        this.memberId = memberId;
+        super(name);  // Call the User class constructor to set the name
+        this.memberId = memberId;  // Initialize the member's unique ID
     }
 
+    /**
+     * Gets the unique ID of the member.
+     * 
+     * @return The member's ID.
+     */
     @Override
     public int getId() {
         return memberId;
     }
 }
 
+/**
+ * The Employee class represents an employee of the library, inheriting from the User class
+ * and implementing the FetchId interface to provide a unique employee ID.
+ */
 class Employee extends User implements FetchId {
+    // Unique ID for the employee
     private int employeeId;
 
+    // Constructor to create an Employee object with just the name (used when the employeeId is not yet known).
+     
     public Employee(String name) {
-        super(name);}
-
-    public Employee(int employeeId, String name) {
-        super(name);
-        this.employeeId = employeeId;
+        super(name);  // Call the User class constructor to set the name
     }
 
+    // Constructor to create an Employee object with both the employee ID and name.
+    
+    public Employee(int employeeId, String name) {
+        super(name);  // Call the User class constructor to set the name
+        this.employeeId = employeeId;  // Initialize the employee's unique ID
+    }
+
+    /**
+     * Gets the unique ID of the employee.
+     * 
+     * @return The employee's ID.
+     */
     @Override
     public int getId() {
         return employeeId;
