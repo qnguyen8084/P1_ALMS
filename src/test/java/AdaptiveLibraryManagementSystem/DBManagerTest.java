1b683329387a2711eb4827b46c9dbc3623ec71f7
Dhruv Shah <shahdhruvddd@gmail.com>
2024-10-02 09:59:41 +0530

    Update DBManagerTest.java

--- src/test/java/AdaptiveLibraryManagementSystem/DBManagerTest.java
+++ src/test/java/AdaptiveLibraryManagementSystem/DBManagerTest.java
@@ -1,5 +1,3 @@
-
-
 package AdaptiveLibraryManagementSystem;
 
 import java.sql.Connection;
@@ -10,14 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DBManagerTest {
 
+    // Test case for establishing a connection to the database
     @org.junit.jupiter.api.Test
     void connect() throws SQLException, ClassNotFoundException {
+        // Establish a connection using DBManager
         Connection conn = DBManager.connect();
+        
+        // Assert that the JDBC driver used is SQLite
         assertEquals("SQLite JDBC", conn.getMetaData().getDriverName());
     }
 
+    // Test case for initializing the database
     @org.junit.jupiter.api.Test
     void initializeDatabase() {
+        // This tests the initialization of the DBManager object
         new DBManager();
+        // Further assertions can be added here if necessary
     }
 }
